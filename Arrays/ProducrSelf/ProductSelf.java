7f813bb48780235b14b476b2f99eecb7402fc499
Rahul Pradhan <rahulp0817@gmail.com>
2025-09-14 01:40:50 +0530

    productself

---
+++ Arrays/ProducrSelf/ProductSelf.java
@@ -0,0 +1,24 @@
+// Leetcode 238
+package Arrays.ProducrSelf;
+
+public class ProductSelf {
+    public int[] productExceptSelf(int[] nums) {
+
+      int[] result = new int[nums.length];
+
+      int leftproduct = 1;
+      for(int i = 0; i < nums.length; i++){
+       result[i] = leftproduct;
+       leftproduct *= nums[i];
+      }
+
+      int rightproduct = 1;
+      for(int i = nums.length - 1; i >= 0; i--){
+        result[i] *= rightproduct; 
+        rightproduct *= nums[i];
+      }
+     
+      return result;
+
+    }
+}
