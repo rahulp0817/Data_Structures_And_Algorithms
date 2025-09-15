ed99096b562f377ff857a4d42016222b265565b3
Rahul Pradhan <rahulp0817@gmail.com>
2025-09-16 00:47:45 +0530

    buy sell stock

--- Arrays/BuySellStock/BuySellStock.java
+++ Arrays/BuySellStock/BuySellStock.java
@@ -1,19 +1,19 @@
 package Arrays.BuySellStock;
 
 public class BuySellStock {
-    public int maxProfit(int[] prices) {
-        int buyPrice = prices[0];
-        int maxPrice = 0;
+  public int maxProfit(int[] prices) {
+    int buyPrice = prices[0];
+    int maxPrice = 0;
 
-        for (int i = 1; i < prices.length; i++){
-            if (buyPrice < prices[i]){
-                int profit = prices[i] - buyPrice;
-                maxPrice = Math.max(maxPrice, profit);
-            } else {
-                buyPrice = prices[i];
-            }
+      for (int i = 1; i < prices.length; i++){
+        if (buyPrice < prices[i]){
+          int profit = prices[i] - buyPrice;
+          maxPrice = Math.max(maxPrice, profit);
+        } else {
+          buyPrice = prices[i];
         }
+      }
 
-        return maxPrice;
-    }
+    return maxPrice;
+  }
 }
