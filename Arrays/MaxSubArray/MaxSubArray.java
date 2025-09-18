f0fd8da1f3168795ca264b14db824489b5175a5f
Rahul Pradhan <rahulp0817@gmail.com>
2025-09-19 02:00:55 +0530

    max sub array

---
+++ Arrays/MaxSubArray/MaxSubArray.java
@@ -0,0 +1,16 @@
+package Arrays.MaxSubArray;
+
+public class MaxSubArray {
+  public int maxSubArray(int[] nums) {
+        
+       int maxSum = nums[0];
+       int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+           currentSum = Math.max(nums[i], currentSum + nums[i]);
+           maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
