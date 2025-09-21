b2b0d1bb7cf9af511df6680ba82d351e67aacca7
Rahul Pradhan <rahulp0817@gmail.com>
2025-09-22 04:44:33 +0530

    valid parentheses

---
+++ Strings/ValidParentheses/ValidParentheses.java
@@ -0,0 +1,34 @@
+package Strings.ValidParentheses;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+public boolean isValid(String s) {
+  Stack<Character> stack = new Stack<>();
+    for(char ch:s.toCharArray()){
+      if(ch == '(' || ch == '[' || ch == '{'){
+        stack.push(ch);
+      }
+      else{
+        if(stack.isEmpty()) {
+          return false;
+        }
+
+        char top = stack.pop();
+        if (ch == ')' && top != '(') {
+          return false;
+        }
+
+        if (ch == ']' && top != '[') {
+          return false;
+        }
+
+        if (ch == '}' && top != '{') {
+          return false;
+        }
+      }
+    }
+    
+    return stack.isEmpty();
+  }
+}
